a00d6fc70b5dabb90562df10beb09fdc7e57089b
HaiderAli99888 <122993240+HaiderAli99888@users.noreply.github.com>
2024-05-17 02:22:37 -0400

    Played with constructors and classes to see their different functionalities.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,63 +1,52 @@
 ﻿using System;
 
+
 namespace HelloWorld
 {
+    class Employee
+    {
+        public string FirstName;
+        public string LastName;
+        public string Email;
+
+        public Employee()
+        {
+            Console.WriteLine("hehehhehe");
+        }
+        public Employee(string checking)
+        {
+            Console.WriteLine(checking);
+        }
+        public void Message() 
+        {
+            Console.WriteLine("{0} {1}",FirstName, LastName);
+        }
+        
+
+    }
+    class FullTimeEmployee: Employee
+    {
+        public int YearlySalary;
+        public FullTimeEmployee() : base("mepaul")
+        { }
+    }
+    class PartTimeEmployee: Employee
+    {
+        public int HourlySalary;
+    }
 
     class Program
     {
 
         static void Main(string[] args)
         {
-            string userChoice;
-            string againChoice;
-            int totalPrice = 0;
-            do
-            {
-               
-                do
-                {
-                    Console.Write("Please enter coffee size, Sir, : 1 for Small, 2 for Medium, 3 for Large: ");
-                    userChoice = (Console.ReadLine());
-
-                    switch (userChoice)
-                    {
-                        case "1":
-                            totalPrice = totalPrice + 1;
-                            break;
-                        case "2":
-                            totalPrice = totalPrice + 2;
-                            break;
-                        case "3":
-                            totalPrice = totalPrice + 3;
-                            break;
-                        default:
-                            Console.WriteLine("Please enter a valid size");
-                            break;
-                    }
-                }
-                while (userChoice != "1" && userChoice != "2" && userChoice != "3");
-
-                do {
-                    Console.WriteLine("Do you want another coffee? ");
-                    againChoice = Console.ReadLine().ToUpper();
-                    switch (againChoice)
-                    {
-                        case "YES":
-                            break;
-                        case "NO":
-                            break;
-                        default:
-                            Console.WriteLine("Invalid choice");
-                            break;
-
-                    }
-                   
-                }
-                while(againChoice!="YES" && againChoice != "NO");
-           
-            }
-            while (againChoice != "NO");
-            Console.WriteLine("Total cost is {0}$", totalPrice);
+            FullTimeEmployee fte=new FullTimeEmployee();
+            fte.FirstName = "Haider";
+            fte.LastName = "Ali";
+            fte.Message();
         }
+
+
     }
+   
 }
