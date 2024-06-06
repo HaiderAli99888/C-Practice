3ea8a2187aee0540a0ebfb1a12b6a650de899193
HaiderAli99888 <122993240+HaiderAli99888@users.noreply.github.com>
2024-06-06 00:58:27 -0400

    indexes in c#

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,43 +3,61 @@ using System.Diagnostics;
 using System.IO;
 using System.Security.Cryptography.X509Certificates;
 using Microsoft.VisualBasic;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Reflection;
+using System.Text;
+using ConsoleApp1;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Runtime.CompilerServices;
 
-class Program
+namespace Haider
+{
+    class Program
     {
-    static void Main(string[] args)
-    {
-        Customer[] customer = new Customer[3];
-        customer[0] = new Customer { Name= "Mark", Gender= Gender.Male};
-        customer[1] = new Customer { Name = "Mary", Gender = Gender.Female };
-        customer[2] = new Customer { Name = "Sam", Gender = Gender.Unknown };
-        foreach(Customer cust in customer)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Name = {0} and Gender = {1}",cust.Name,GenderConverter(cust.Gender));
+            Company company = new Company();
+            Console.WriteLine("Name of Employee with Id = 2: " + company[2]);
+            company[1] = "Haider";
+            Console.WriteLine("Name of Employee with Id = 1: " + company[1]);
+
+        }
+
+    }
+    public class Employee
+    {
+        public int EmployeeId { get; set; }   
+        public string Name { get; set; }
+        public string Gender { get; set; }
+    }
+    public class Company
+    {
+        public List<Employee> listEmployees= new List<Employee>();
+        public Company()
+        {
+            listEmployees.Add(new Employee { EmployeeId=1 ,Name="mack",Gender="MALE" });
+            listEmployees.Add(new Employee { EmployeeId = 2, Name = "mack2", Gender = "MALE" });
+            listEmployees.Add(new Employee { EmployeeId = 3, Name = "mack3", Gender = "MALE" });
+            listEmployees.Add(new Employee { EmployeeId = 4, Name = "mack4", Gender = "MALE" });
+            listEmployees.Add(new Employee { EmployeeId = 5, Name = "mack5", Gender = "MALE" });
+        }
+        public string this[int employeeId]
+        {
+            get
+            {
+                return listEmployees.FirstOrDefault(emp => emp.EmployeeId == employeeId).Name;
+            }
+            set
+            {
+                listEmployees.FirstOrDefault(emp => emp.EmployeeId == employeeId).Name=value;
+
+            }
         }
     }
-    public static string GenderConverter(Gender gender) 
-    {
-        switch (gender)
-        {
-            case Gender.Male:
-                return "male";
-                case Gender.Female:
-                return "Female";
-                case Gender.Unknown:
-                return "unknown";
-            default:
-                return "invalid";
-        }
-    }
+
+  
 }
-public enum Gender
-{
-    Unknown,
-    Male,
-    Female
-}
-public class Customer
-{
-    public string Name { get; set; }
-    public Gender Gender {  get; set; }
-}
+
