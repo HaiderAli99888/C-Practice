054c9e983356915d4f902dd4eaef83bdcc63d087
HaiderAli99888 <122993240+HaiderAli99888@users.noreply.github.com>
2024-06-09 23:58:21 -0400

    List Sorting

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,40 +11,87 @@ namespace Haider
             {
                 ID = 121,
                 Name = "Ali",
-                Salary = 5000
+                Salary = 5500
             };
 
             Customr customer2 = new Customr()
             {
-                ID = 140,
+                ID = 130,
                 Name = "Ahmed",
-                Salary = 4500
+                Salary = 6500
             };
 
             Customr customer3 = new Customr()
             {
                 ID = 119,
                 Name = "Mili",
-                Salary = 3500
+                Salary = 4500
             };
-
-            Customr[] cus = new Customr[3];
-                cus[0] = customer1;
-            cus[1]= customer2;
-            cus[2]= customer3;
-            Dictionary<int, Customr> diss = cus.ToDictionary(cust => cust.ID, cust => cust);
-            foreach(KeyValuePair<int, Customr> kvp in diss)
+        
+            List<Customr> customerList = new List<Customr>();
+            customerList.Add(customer1);
+            customerList.Add(customer2);
+            customerList.Add(customer3);
+            Console.WriteLine("BEFORE SORT");
+            foreach (Customr cust in customerList)
             {
-                Customr info = kvp.Value;
-                Console.WriteLine(info.Name);
+                Console.WriteLine(cust.ID);
             }
+            customerList.Sort();
+            Console.WriteLine("AFTER SORT");
+            foreach (Customr cust in customerList)
+            {
+                Console.WriteLine(cust.ID);
+            }
+          
+            Console.WriteLine("bEFORE NAME SORTING");
+            foreach (Customr cust in customerList)
+            {
+                Console.WriteLine(cust.Name);
+            }
+            Console.WriteLine("aFTER NAME SORTING");
+
+            SortByName byName = new SortByName();
+            customerList.Sort(byName);
+            foreach (Customr cust in customerList)
+            {
+                Console.WriteLine(cust.Name);
+            }
+            Console.WriteLine("bEFORE NAME SORTING WITHOUT USING DELEGAT");
+            foreach (Customr cust in customerList)
+            {
+                Console.WriteLine(cust.Salary);
+            }
+            customerList.Sort((x,y)=>x.Salary.CompareTo(y.Salary));
+            Console.WriteLine("aFTER NAME SORTING USING DELEGATE");
+
+            foreach (Customr cust in customerList)
+            {
+                Console.WriteLine(cust.Salary);
+            }
+
+
         }
     }
 
-    public class Customr
+    public class SortByName : IComparer<Customr>
+    {   
+        public int Compare(Customr x, Customr y)
+            {
+                return x.Name.CompareTo(y.Name);
+            }
+          
+    }
+    public class Customr:IComparable<Customr>
     {
         public int ID { get; set; }
         public string Name { get; set; }
         public int Salary { get; set; }
+        public int CompareTo(Customr other)
+        {
+            return this.ID.CompareTo(other.ID);
+        }
     }
+   
+
 }
