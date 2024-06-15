db274088c45aa7f23d271fa1fc34b86bae0edd74
HaiderAli99888 <122993240+HaiderAli99888@users.noreply.github.com>
2024-06-15 01:09:48 -0400

    Async and Await practice

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,103 +1,34 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
 
 public class Program
 {
+    private static int CountCharacters()
+    {
+        int count = 0;
+        using (StreamReader reader = new StreamReader("C:\\data\\data.txt"))
+        {
+            string content = reader.ReadToEnd();
+            count = content.Length;
+            Thread.Sleep(5000); // Simulating long processing time
+        }
+        return count;
+    }
+
+    public async static Task AA()
+    {
+        Task<int> task = new Task<int>(CountCharacters);
+        task.Start();
+
+        Console.WriteLine("Processing file. Please wait...");
+        int count = await task;
+        Console.WriteLine(count.ToString() + " characters in file");
+    }
+
     public static void Main()
     {
-        Console.WriteLine("Main Started");
-        Account accountA = new Account(101, 5000);
-        Account accountB = new Account(102, 3000);
-
-        AccountManager accountManagerA = new AccountManager(accountA, accountB, 1000);
-        Thread T1 = new Thread(accountManagerA.Transfer);
-        T1.Name = "T1";
-
-        AccountManager accountManagerB = new AccountManager(accountB, accountA, 2000);
-        Thread T2 = new Thread(accountManagerB.Transfer);
-        T2.Name = "T2";
-
-        T1.Start();
-        T2.Start();
-
-        T1.Join();
-        T2.Join();
-
-        Console.WriteLine("Main Completed");
+        AA().GetAwaiter().GetResult();
     }
 }
-
-public class Account
-{
-    double _balance;
-    int _id;
-
-    public Account(int id, double balance)
-    {
-        this._id = id;
-        this._balance = balance;
-    }
-
-    public int ID
-    {
-        get { return _id; }
-    }
-
-    public void Withdraw(double amount)
-    {
-        _balance = _balance-amount;
-    }
-
-    public void Deposit(double amount)
-    {
-        _balance = _balance - amount;
-    }
-}
-public class AccountManager
-{
-    Account _fromAccount;
-    Account _toAccount;
-    double _amountToTransfer;
-
-    public AccountManager(Account fromAccount, Account toAccount, double amountToTransfer)
-    {
-        this._fromAccount = fromAccount;
-        this._toAccount = toAccount;
-        this._amountToTransfer = amountToTransfer;
-    }
-
-    public void Transfer()
-    {
-        object _lock1, _lock2;
-
-        if (_fromAccount.ID < _toAccount.ID)
-        {
-            _lock1 = _fromAccount;
-            _lock2 = _toAccount;
-        }
-        else
-        {
-            _lock1 = _toAccount;
-            _lock2 = _fromAccount;
-        }
-
-        Console.WriteLine(Thread.CurrentThread.Name + " trying to acquire lock on " + ((Account)_lock1).ID.ToString());
-
-        lock (_lock1)
-        {
-            Console.WriteLine(Thread.CurrentThread.Name + " acquired lock on " + ((Account)_lock1).ID.ToString());
-            Console.WriteLine(Thread.CurrentThread.Name + " suspended for 1 second");
-            Thread.Sleep(1000);
-            Console.WriteLine(Thread.CurrentThread.Name + " back in action and trying to acquire lock on " + ((Account)_lock2).ID.ToString());
-
-            lock (_lock2)
-            {
-                Console.WriteLine(Thread.CurrentThread.Name + " acquired lock on " + ((Account)_lock2).ID.ToString());
-                _fromAccount.Withdraw(_amountToTransfer);
-                _toAccount.Deposit(_amountToTransfer);
-                Console.WriteLine(Thread.CurrentThread.Name + " Transferred " + _amountToTransfer.ToString() + " from " + _fromAccount.ID + " to " + _toAccount.ID);
-            }
-        }
-    }
-}
-
